a835f798826e44d05aa0122a98dd36b1bd3e02b6
My-ABC <13020206640@163.com>
2026-07-09 16:36:24 +0800

    impl: io.h

---
+++ include/io.h
@@ -0,0 +1,15 @@
+#ifndef _IO_H
+#define _IO_H
+
+#include "stdint.h"
+
+inline void outb(uint16_t port, uint8_t val);
+inline uint8_t inb(uint16_t port);
+
+inline void outw(uint16_t port, uint16_t val);
+inline uint16_t inw(uint16_t port);
+
+inline void outl(uint32_t port, uint32_t val);
+inline uint32_t inl(uint16_t port);
+
+#endif
